f4bd691ee1866641b13a1ffe7c86423b39841c29
SerjLeo <sergejleontev111@gmail.com>
2022-03-20 22:11:35 +0300

    working on register form

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import useFormInput from "../../hooks/useFormInput";
+import {Button, TextField} from "@mui/material";
+import { Link } from 'react-router-dom';
 
-const Register = () => {
+const Register = React.memo(() => {
+    const {getFormFieldProps, onFormSubmit} = useFormInput({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    console.log('render')
+
+    const handleSubmit = (form: Record<string, unknown>) => {
+        console.log(form)
+    }
+
     return (
         <div>
-            register
+            <h2>Registration</h2>
+            <form onSubmit={onFormSubmit(handleSubmit)}>
+                <TextField label='name' {...getFormFieldProps('name')}/>
+                <TextField label='email' {...getFormFieldProps('email')}/>
+                <TextField label='password' {...getFormFieldProps('password')}/>
+                <Button type="submit">Submit</Button>
+                <Link to="/login">Already registered? Login!</Link>
+            </form>
         </div>
     );
-};
+});
 
 export default Register;
