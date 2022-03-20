a17850929c69164e58ff939f0e8e7a8749e37c22
SerjLeo <sergejleontev111@gmail.com>
2022-03-20 22:09:49 +0300

    add error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 
 ReactDOM.render(
   <React.StrictMode>
       <BrowserRouter>
-            <App/>
+          <ErrorBoundary>
+              <App/>
+          </ErrorBoundary>
       </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
