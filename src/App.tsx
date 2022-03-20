b07ea518f6c3b6c8edcab806876857269b999e4b
SerjLeo <sergejleontev111@gmail.com>
2022-03-20 22:10:22 +0300

    layout styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AppRouter from "./router/AppRouter";
 import "./assets/styles/app.scss"
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
   return (
     <div className="App">
+        <CssBaseline />
         <AppRouter/>
     </div>
   );
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,14 +2,17 @@ import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from "../components/Header/Header";
 import Spinner from "../components/Spinner/Spinner";
+import LayoutsModule from "./styles/Layouts.module.scss"
 
 const MainLayout: React.FunctionComponent = () => {
     return (
-        <div>
+        <div className={LayoutsModule.app__container}>
             <Header/>
-            <Suspense fallback={<Spinner/>}>
-                <Outlet/>
-            </Suspense>
+            <div className={LayoutsModule.app__content}>
+                <Suspense fallback={<Spinner/>}>
+                    <Outlet/>
+                </Suspense>
+            </div>
         </div>
     );
 };
