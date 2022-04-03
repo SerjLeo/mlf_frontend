27f2f57857d908c95f82283f47394c3412666bdd
SerjLeo <sergejleontev111@gmail.com>
2022-04-03 21:26:19 +0300

    add category reducers

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,10 +1,14 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import userReducer from "./user"
+import categoriesListReducer from "./categoriesList"
+import categoryReducer from "./categoryPage"
 import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
-    user: userReducer
+    user: userReducer,
+    categoriesList: categoriesListReducer,
+    category: categoryReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
