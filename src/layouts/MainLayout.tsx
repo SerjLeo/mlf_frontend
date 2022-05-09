ab1da6af6b1d42a5110689292eb97d9f05f6d052
SerjLeo <sergejleontev111@gmail.com>
2022-05-09 16:47:40 +0300

    Remove header from main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,13 +1,11 @@
 import React, { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from '../components/Header/Header'
 import Spinner from '../components/Spinner/Spinner'
 import LayoutsModule from './styles/Layouts.module.scss'
 
 const MainLayout: React.FunctionComponent = () => {
 	return (
 		<div className={LayoutsModule.app__container}>
-			<Header/>
 			<div className={LayoutsModule.app__content}>
 				<Suspense fallback={<Spinner/>}>
 					<Outlet/>
