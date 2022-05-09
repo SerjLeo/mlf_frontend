45af4e7de3f5ae69741e14e955b9c49effb07ed4
SerjLeo <sergejleontev111@gmail.com>
2022-05-09 18:22:12 +0300

    styling create transaction form

--- src/components/CrateTransactionForm/CreateTransactionForm.tsx
+++ src/components/CrateTransactionForm/CreateTransactionForm.tsx
@@ -43,39 +43,41 @@ const CreateTransactionForm: React.FC<CreateTransactionFormProps> = ({
 	return (
 		<div className={styles.transaction__form__wrap}>
 			<form onSubmit={onFormSubmit(handleSubmit)} className={styles.transaction__form}>
-				<div className={styles.form__inputs_group}>
-					<TextField
-						size="small"
-						label='amount'
-						variant="standard"
-						type="number"
-						inputProps={{min: 0, max: 1000000}}
-						{...getFormFieldProps('amount', numberConverter, [maxNumberValue(1000000), minNumberValue(0)])}
-						required className={styles.form__input}
-					/>
-					<TextField
-						size="small"
-						variant="standard"
-						label='description'
-						inputProps={{maxLength: 75}}
-						{...getFormFieldProps('description')}
-						className={styles.form__input}
-					/>
-				</div>
 				<div className={styles.form__inputs_controls}>
 					<div className={styles.transaction__form__switch}>
 						<button
 							className={`${styles.form__switch__btn} ${form.type && styles.active} ${styles.switch__btn__positive}`}
 							onClick={() => setType(true)}
 							type='button'
-							data-testid="positive-type-btn"><AddIcon/></button>
+							data-testid="positive-type-btn"><AddIcon fontSize="small"/></button>
 						<button
 							className={`${styles.form__switch__btn} ${!form.type && styles.active} ${styles.switch__btn__negative}`}
 							onClick={() => setType(false)}
 							type='button'
-							data-testid="negative-type-btn"><RemoveIcon/></button>
+							data-testid="negative-type-btn"><RemoveIcon fontSize="small"/></button>
 					</div>
-					<Button disabled={loading} type="submit">ADD</Button>
+					<div>
+						<Button disabled={loading} size="small" type="submit">ADD</Button>
+					</div>
+				</div>
+				<div className={styles.form__inputs_group}>
+					<TextField
+						size="small"
+						label='amount'
+						variant="outlined"
+						type="number"
+						inputProps={{min: 0, max: 1000000}}
+						{...getFormFieldProps('amount', numberConverter, [maxNumberValue(1000000), minNumberValue(0)])}
+						required className={styles.form__input_amount}
+					/>
+					<TextField
+						size="small"
+						variant="outlined"
+						label='description'
+						inputProps={{maxLength: 75}}
+						{...getFormFieldProps('description')}
+						className={styles.form__input_description}
+					/>
 				</div>
 			</form>
 		</div>
